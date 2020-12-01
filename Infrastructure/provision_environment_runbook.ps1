3fa7eb1575d067c791b0657c7ff5fef998e6bbe4
martin <28552348+cairneym@users.noreply.github.com>
2020-12-01 20:11:51 +1100

    fixed another region default

--- Infrastructure/provision_environment_runbook.ps1
+++ Infrastructure/provision_environment_runbook.ps1
@@ -1,7 +1,7 @@
 param(
     $awsAccessKey = "",
     $awsSecretKey = "",
-    $defaulAwsRegion = "eu-west-1", # Other carbon neutral regions are listed here: https://aws.amazon.com/about-aws/sustainability/
+    $defaulAwsRegion = "ap-southeast-2", # Other carbon neutral regions are listed here: https://aws.amazon.com/about-aws/sustainability/
     $securityGroupName = "RandomQuotes_SQL",
     $numWebServers = 1,
     $instanceType = "t2.micro", # 1 vCPU, 1GiB Mem, free tier elligible: https://aws.amazon.com/ec2/instance-types/
